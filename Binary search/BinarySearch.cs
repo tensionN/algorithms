9a08d09b46ec9a714ca8e6ac40cabcc8257a99ab
Oleksandr <ssolomiychuks@gmail.com>
2022-10-25 22:42:00 +0300

    binary search

---
+++ Binary search/BinarySearch.cs
@@ -0,0 +1,16 @@
+﻿int BinarySearch(int[] inputArray, int key)  
+{ 
+    int min = 0;
+    int max = inputArray.Length - 1; 
+    while (min <=max)  
+    {  
+        int mid = (min + max) / 2;  
+        if (key == inputArray[mid])
+            return mid;
+        else if (key < inputArray[mid])
+            max = mid - 1;
+        else
+            min = mid + 1;
+    }  
+    return -1;  
+}  
